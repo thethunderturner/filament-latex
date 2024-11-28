5b4b13dda6cf92340889f4c917fe6cf41feb59b9
Matthaios Biskas <m.biskas@student.tue.nl>
2024-11-28 16:05:32 +0100

    removing redundant loc

--- src/FilamentLatexServiceProvider.php
+++ src/FilamentLatexServiceProvider.php
@@ -3,11 +3,7 @@
 namespace TheThunderTurner\FilamentLatex;
 
 use Filament\Support\Assets\AlpineComponent;
-use Filament\Support\Assets\Asset;
-use Filament\Support\Assets\Css;
-use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
-use Filament\Support\Facades\FilamentIcon;
 use Livewire\Livewire;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -16,8 +12,6 @@ class FilamentLatexServiceProvider extends PackageServiceProvider
 {
     public static string $name = 'filament-latex';
 
-    public static string $viewNamespace = 'filament-latex';
-
     public function configurePackage(Package $package): void
     {
         $package->name(static::$name)
@@ -25,75 +19,15 @@ class FilamentLatexServiceProvider extends PackageServiceProvider
             ->hasTranslations();
     }
 
-    public function packageRegistered(): void {}
-
     public function packageBooted(): void
     {
         Livewire::component('filament-latex', FilamentLatex::class);
 
-        // Asset Registration
         FilamentAsset::register(
-            $this->getAssets(),
-            $this->getAssetPackageName()
+            assets: [
+                AlpineComponent::make('filament-latex', __DIR__ . '/../resources/dist/filament-latex.js'),
+            ],
+            package: 'thethunderturner/filament-latex'
         );
-
-        FilamentAsset::registerScriptData(
-            $this->getScriptData(),
-            $this->getAssetPackageName()
-        );
-
-        // Icon Registration
-        FilamentIcon::register($this->getIcons());
-    }
-
-    protected function getAssetPackageName(): ?string
-    {
-        return 'thethunderturner/filament-latex';
-    }
-
-    /**
-     * @return array<Asset>
-     */
-    protected function getAssets(): array
-    {
-        return [
-            // AlpineComponent::make('filament-latex', __DIR__ . '/../resources/dist/components/filament-latex.js'),
-            Css::make('filament-latex-styles', __DIR__ . '/../resources/dist/filament-latex.css'),
-            Js::make('filament-latex-scripts', __DIR__ . '/../resources/dist/filament-latex.js'),
-        ];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getIcons(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getRoutes(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string, mixed>
-     */
-    protected function getScriptData(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getMigrations(): array
-    {
-        return [
-            'create_filament-latex_table',
-        ];
     }
 }
