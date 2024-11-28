341e8e7d0e26561f714c8dfd139b1cf813f1ad29
Matthaios Biskas <m.biskas@student.tue.nl>
2024-11-28 23:54:45 +0100

    added missing packages replaced hello world

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,14 +1,16 @@
 import { EditorView, basicSetup } from 'codemirror'
 import { javascript } from '@codemirror/lang-javascript'
 import { EditorState } from '@codemirror/state'
+import {defaultKeymap} from "@codemirror/commands"
+import { keymap } from '@codemirror/view'
 
 export default function codeEditor() {
     return {
         init() {
             new EditorView({
                 state: EditorState.create({
-                    doc: 'Hello World',
-                    extensions: [basicSetup, javascript()]
+                    doc: '\\documentclass{article}\n\\begin{document}\nHello LaTeX\n\\end{document}',
+                    extensions: [basicSetup, keymap.of(defaultKeymap), javascript()]
                 }),
                 parent: this.$el
             })
