f08e87ba770cb5b69431e4f8c65eceac136a4aef
Matthew Biskas <matthewbiskas@gmail.com>
2025-07-02 20:23:17 +0200

    allow multiple files, with distinct names

--- src/Concerns/CanUploadFiles.php
+++ src/Concerns/CanUploadFiles.php
@@ -4,6 +4,8 @@ namespace TheThunderTurner\FilamentLatex\Concerns;
 
 use Filament\Actions\Action;
 use Filament\Forms\Components\FileUpload;
+use Illuminate\Support\Facades\Storage;
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 
 trait CanUploadFiles
 {
@@ -32,7 +34,33 @@ trait CanUploadFiles
                     ->disk(config('filament-latex.storage'))
                     ->directory($this->filamentLatex->id . '/files')
                     ->visibility('private')
-                    ->preserveFilenames(),
+                    ->preserveFilenames()
+                    ->multiple()
+                    ->getUploadedFileNameForStorageUsing(function (TemporaryUploadedFile $file) {
+                        $storage = Storage::disk(config('filament-latex.storage'));
+                        $directory = $this->filamentLatex->id . '/files';
+                        $originalName = $file->getClientOriginalName();
+                        $path = $directory . '/' . $originalName;
+
+                        if (! $storage->exists($path)) {
+                            return $originalName;
+                        }
+
+                        $extension = pathinfo($originalName, PATHINFO_EXTENSION);
+                        $filename = pathinfo($originalName, PATHINFO_FILENAME);
+                        $counter = 1;
+
+                        while (true) {
+                            $newFilename = $filename . ' (' . $counter . ')' . ($extension ? '.' . $extension : '');
+                            $newPath = $directory . '/' . $newFilename;
+
+                            if (! $storage->exists($newPath)) {
+                                return $newFilename;
+                            }
+
+                            $counter++;
+                        }
+                    }),
             ]);
     }
 
